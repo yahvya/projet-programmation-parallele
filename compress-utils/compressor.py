058756784d30eced2fa5c0fe130256e89aedcff8
Bathily Yahaya <developpeur.yb@gmail.com>
2024-02-27 11:05:38 +0100

    correction de la création de l'arbre fin d'implémentation de la fonction d'associtation du binaire

--- compress-utils/compressor.py
+++ compress-utils/compressor.py
@@ -18,8 +18,7 @@ class HuffmanCompressor:
     def compress_file(file_path: str) -> str:
         try:
             with open(file_path, 'r') as file_to_compress:
-                return HuffmanCompressor.compress_tree(HuffmanCompressor.get_tree_from_file(file_to_compress),
-                                                       file_to_compress)
+                return HuffmanCompressor.compress_tree(HuffmanCompressor.get_tree_from_file(file_to_compress), file_to_compress)
 
         except CompressionException as e:
             raise e
@@ -34,6 +33,7 @@ class HuffmanCompressor:
     # @throws CompressionException en cas d'erreur
     @staticmethod
     def compress_tree(tree: [str, ], file: TextIO) -> str:
+        print(tree)
         pass
 
     ##
@@ -43,22 +43,23 @@ class HuffmanCompressor:
     # @throws CompressionException en cas d'erreur
     @staticmethod
     def get_tree_from_file(file: TextIO) -> [str, ]:
-        arr = [1,2,3,4]
-
-        tmp_tree = Counter([*file.read()])
+        tmp_tree: [str, int] = Counter([*file.read()])
 
         if len(tmp_tree) == 0:
-            raise CompressionException("Le fichier est vide")
+            return {}
 
-        node_arr = [{"count": tmp_tree[el], "val": el, "binary": ""} for el in tmp_tree]
+        # transformation des associations occurrences en tableau de nœuds de l'arbre
+        node_arr: [[str,]] = [{"count": tmp_tree[el], "val": el, "binary": ""} for el in tmp_tree]
 
         while len(node_arr) > 1:
-
-            # on défini les deux noeuds les plus petits par défaut (smaller one doit être plus petit que smaller two)
+            # nous définissons les deux nœuds les plus petits par défaut (smaller one doit être plus petit que smaller two)
             smaller_one = 0 if node_arr[0]["count"] < node_arr[1]["count"] else 1
             smaller_two = 1 if smaller_one == 0 else 0
 
-            for index in range(0, len(node_arr) - 1):
+            for index in range(0, len(node_arr)):
+                if index == smaller_one:
+                    continue
+
                 node = node_arr[index]
 
                 # récupération de l'élément le plus petit
@@ -81,15 +82,31 @@ class HuffmanCompressor:
 
             node_arr.append(new_node)
 
+        HuffmanCompressor.create_binary_for_tree(node_arr[0])
+
         return node_arr[0]
+
     ##
     # @brief associe les binaires dans l'arbre
     # @param tree l'arbre
     # @param binary le binaire à associer
+    # @param left_code code associé en descendant du côté gauche de l'arbre
     @staticmethod
-    def create_binary_for_tree(tree: [str, ], binary: str = ""):
-        pass
+    def create_binary_for_tree(tree: [str, ], binary: str = "", left_code: str = "0"):
+        if not ("binary" in tree):
+            right_code = "1" if left_code == "0" else "0"
+            HuffmanCompressor.create_binary_for_tree(tree["val"]["left"], f"{binary}{left_code}")
+            HuffmanCompressor.create_binary_for_tree(tree["val"]["right"], f"{binary}{right_code}")
+        else:
+            tree["binary"] = binary
+
+    ##
+    # @brief affiche l'arbre donné
+    # @param tree l'arbre à afficher
+    @staticmethod
+    def print_tree(tree: [str,]):
+        print(json.dumps(tree, indent=2))
 
 
-HuffmanCompressor.compress_file(
-    "C:/Users/devel/Desktop/fac/master/master-1-dev-web-mobile-fullstack/semestre-2/programmation-parrallele-et-distribue/projet/resources/a-faire-morpheus.txt")
+# HuffmanCompressor.compress_file("C:/Users/devel/Desktop/fac/master/master-1-dev-web-mobile-fullstack/semestre-2/programmation-parrallele-et-distribue/projet/resources/test.txt")
+HuffmanCompressor.compress_file("C:/Users/devel/Desktop/fichiers-temporaires/a-faire-morpheus.txt")
